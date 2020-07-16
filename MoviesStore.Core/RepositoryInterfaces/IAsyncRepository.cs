f2b864d96ebd0dcb9a3dde0ca9c8233b5484d920
狄晖 黄 <kobedddh@gmail.com>
2020-07-15 23:35:48 -0400

    testing

---
+++ MoviesStore.Core/RepositoryInterfaces/IAsyncRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MoviesStore.Core.RepositoryInterfaces
+{
+    //by default Interface is public, and cannot use access modifer
+    interface IAsyncRepository<T> where T:class
+    {
+        //base interface with all our CRUD operations
+        T GetById(int id);
+        IEnumerable<T> ListAll();
+        IEnumerable<T> ListWhere(Expression<Func<T, bool>> filter);
+        int GetCount(Expression<Func<T, bool>> filter = null);
+        bool GetExists(Expression<Func<T, bool>> filter = null);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
